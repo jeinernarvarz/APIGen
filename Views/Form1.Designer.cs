5160be57ad992cebfd14a4db76e27918a0720e65
Paulinarg13 <148563322+Paulinarg13@users.noreply.github.com>
2024-05-23 16:17:07 -0500

    Add project files.

---
+++ Views/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace APIGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxIMG = new System.Windows.Forms.PictureBox();
+            this.buttonGEN = new System.Windows.Forms.Button();
+            this.labelTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIMG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxIMG
+            // 
+            this.pictureBoxIMG.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxIMG.Location = new System.Drawing.Point(25, 65);
+            this.pictureBoxIMG.Name = "pictureBoxIMG";
+            this.pictureBoxIMG.Size = new System.Drawing.Size(814, 554);
+            this.pictureBoxIMG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxIMG.TabIndex = 0;
+            this.pictureBoxIMG.TabStop = false;
+            // 
+            // buttonGEN
+            // 
+            this.buttonGEN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGEN.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGEN.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonGEN.Location = new System.Drawing.Point(340, 631);
+            this.buttonGEN.Name = "buttonGEN";
+            this.buttonGEN.Size = new System.Drawing.Size(165, 38);
+            this.buttonGEN.TabIndex = 1;
+            this.buttonGEN.Text = "RANDOM";
+            this.buttonGEN.UseVisualStyleBackColor = true;
+            this.buttonGEN.Click += new System.EventHandler(this.buttonGEN_Click);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(31, 30);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(142, 28);
+            this.labelTitle.TabIndex = 2;
+            this.labelTitle.Text = "IMG RANDOM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(863, 682);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.buttonGEN);
+            this.Controls.Add(this.pictureBoxIMG);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = " API Random IMG ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIMG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxIMG;
+        private System.Windows.Forms.Button buttonGEN;
+        private System.Windows.Forms.Label labelTitle;
+    }
+}
+
---
+++ Views/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace APIGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //tooltip pa mostrar en el btn de Buscar/Actualizar/Eliminar/Agregar se pa que hace el boton
+            System.Windows.Forms.ToolTip toolTip1 = new System.Windows.Forms.ToolTip();
+            toolTip1.SetToolTip(buttonGEN, " Generar una imagen ");
+        }
+        // Método buttonGEN_Click()
+        private async void buttonGEN_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia de HttpClient para realizar solicitudes HTTP
+            using (HttpClient client = new HttpClient())
+            {
+                // Enviar una solicitud GET a la URL  para obtener una imagen aleatoria
+                HttpResponseMessage response = await client.GetAsync("https://source.unsplash.com/random");
+                // Verificar si la solicitud fue exitosa
+                if (response.IsSuccessStatusCode)
+                {
+                    // Obtener la URL de la imagen directamente de la solicitud original
+                    string imageUrl = response.RequestMessage.RequestUri.ToString();
+                    // Descargar la imagen y mostrarla en el PictureBox
+                    using (HttpClient imageClient = new HttpClient())
+                    {
+                        // Descargar la imagen utilizando otra instancia de HttpClient y convertirla en un arreglo de bytes
+                        byte[] imageData = await imageClient.GetByteArrayAsync(imageUrl);            
+                        // Crear un MemoryStream con los datos de la imagen
+                        using (var ms = new System.IO.MemoryStream(imageData))
+                        {
+                            // Mostrar la imagen descargada en el control PictureBox del formulario
+                            pictureBoxIMG.Image = Image.FromStream(ms);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
